f06640973220ac1e75de9251d811b93ccb3d0cb0
renanz <renan_zelaya@hotmail.com>
2018-10-25 22:16:57 -0600

    CRUD done

---
+++ models/company.model.js
@@ -0,0 +1,15 @@
+//import the mongoose package
+const mongoose = require('mongoose');
+//get the Schema class
+const Schema = mongoose.Schema;
+
+const CompanySchema = new Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    city: String,
+    address: String
+});
+
+module.exports = mongoose.model('Company', CompanySchema);
